dbee172dbcb2c6ab8e46af94bac1d496b62f1f4a
andrego50 <31556122+andrego50@users.noreply.github.com>
2023-02-21 17:44:16 -0500

    con shp

--- Microzone multicrime function.R
+++ Microzone multicrime function.R
@@ -1,8 +1,18 @@
 ## Microzone multicrime
 
-departamento <- ''
+# Set working directory
+setwd(dirname(rstudioapi::getSourceEditorContext()$path))
 
+# Write the deparment name
+departament <- 'TOLIMA'
+# Write the municipalities names
+municipalities <- c('IBAGUÉ', 'CAJAMARCA', 'PIEDRAS', 'ALVARADO')
+# Import criminal data
+data <- ''
 
+# Run function
+multicrime <- function(data, departament, municipalities) {
+  
 # Required Packages
 my_packages <- c('ks', 'sf', 'mapview', 'dplyr', 'readr')
 not_installed <- my_packages[!(my_packages %in% installed.packages()[ , "Package"])]
@@ -12,4 +22,16 @@ for (q in 1:length(my_packages)) {
 }
 
 # Load of the municipalities polygons
+municipios <- st_read("mun_13_02_2018/Municipios.shp")
+municipios <- st_as_sf(municipios, crs = 4326)
+unidad <- municipios[municipios$NOM_DEPART == departament,]
+unidad <- unidad[unidad$NOM_MUNICI %in% municipalities,]
+unidad <- st_transform(unidad, 4326) 
 
+grid <- st_make_grid(unidad, cellsize = 0.01/5)
+grid_kde <- do.call(rbind, st_centroid(grid)) %>% 
+  as_tibble()
+
+# 
+
+}
